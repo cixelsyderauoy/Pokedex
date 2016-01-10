560594d8886dca28d66578bdcd7bb957424685a3
Zachary Silvia <zachary.silvia.zs@gmail.com>
2016-01-10 13:02:35 -0800

    Main Pokemon Screen, Search Bar, and some of pokedex info

---
+++ Pokedex/PokemonDetailVC.swift
@@ -0,0 +1,37 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Zachary Ryan on 1/9/16.
+//  Copyright © 2016 Zachary Ryan. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
